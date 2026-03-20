5704c7218f9dad63c1618bf5674f9dadc4211adb
fortifytech <administrator@fortifytech.co.uk>
2026-03-20 23:06:21 +0200

    Added public dependency

--- TrippyEscapee/Source/TrippyEscapee/TrippyEscapee.Build.cs
+++ TrippyEscapee/Source/TrippyEscapee/TrippyEscapee.Build.cs
@@ -8,7 +8,7 @@ public class TrippyEscapee : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PaperZD" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
